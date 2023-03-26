cf1ee044ed53831eb1011ab867a000ac7d14f0bc
Luan Ferreira <luan.ferreira@clevertech.biz>
2023-03-26 15:48:06 -0300

    Refactor: Update README and gitgpt.zsh

--- README.md
+++ README.md
@@ -1,20 +1,31 @@
-Run
+# Demo
 
+![](https://s2.gifyu.com/images/ezgif.com-cropeffe3d4f0d054178.gif)
+
+# Setup
 
 ```
 chmod +x install.sh
 ./install.sh
 ```
 
-To copy gitgpt into your .zshrc. 
+This will copy gitgpt into your ```.zshrc``` file
 
 Make sure you have the OPENAI_API_KEY environment variable set.
 
-You can do so by running
+You can do so by running:
 
 ```export OPEN_API_KEY=<your key here>```
 
 Or by adding the above command to your .zshrc, or whatever other environment variable setup you prefer. 
 
-If you enjoy this, you might like cligpt - to get suggestions for general Linux commands.
+# Run
+
+```gitgpt <git-related question>```
+
+You will asked to confirm if you want to run the command or not.
+
+## Thanks for being here
+
+If you enjoyed this, you might like cligpt - to get suggestions for general Linux commands.
 
--- gitgpt.zsh
+++ gitgpt.zsh
@@ -2,38 +2,39 @@ gitgpt() {
   OPENAI_API_KEY=$OPENAI_API_KEY
 
   QUERY="$@"
-  PROMPT="Tell me exactly what GIT command I can copy and paste to run in my terminal that will achieve ${QUERY}. Answer a valid GIT command and nothing else."
+  PRMPT="Tell me exactly what GIT command I can copy and paste to run in my terminal that will achieve ${QUERY}. Answer a valid GIT command and nothing else."
 
   RESPONSE=$(curl -s -X POST "https://api.openai.com/v1/chat/completions" \
   -H "Content-Type: application/json" \
   -H "Authorization: Bearer $OPENAI_API_KEY" \
   -d "{
       \"model\": \"gpt-3.5-turbo\",
-      \"messages\": [{\"role\": \"user\", \"content\": \"$PROMPT\"}],
+      \"messages\": [{\"role\": \"user\", \"content\": \"$PRMPT\"}],
       \"temperature\": 0.5,
-      \"max_tokens\": 50
+      \"max_tokens\": 100
   }")
 
   ERROR_MESSAGE=$(echo "$RESPONSE" | jq -r '.error.message // ""')
   COMMAND=$(echo "$RESPONSE" | jq -r '.choices[0].message.content // ""')
 
-  if [ -n "$ERROR_MESSAGE" ]; then
-    echo "Error: $ERROR_MESSAGE"
-  elif [ -n "$COMMAND" ]; then
-		echo "-> $COMMAND"
-  	read -q "REPLY?Do you want to run this command? (y/n)"
-  	echo
-  	if [[ "$REPLY" =~ ^[Yy]$ ]]; then
-    	eval "$COMMAND"
-			eval "zsh"
-			eval "clear"
+	if [ -n "$ERROR_MESSAGE" ]; then
+		echo "Error: $ERROR_MESSAGE"
+	elif [ -n "$COMMAND" ]; then
+		if [[ "$COMMAND" =~ ^git ]]; then
+			echo "-> $COMMAND"
+			read -q "REPLY?Do you want to run this command? (y/n)"
+			if [[ "$REPLY" =~ ^[Yy]$ ]]; then
+				echo
+				eval "$COMMAND"
+			else
+				echo
+				echo "Command not executed."
+			fi
 		else
-			echo "Command not executed."
-			eval "zsh"
-			eval "clear"
+			echo "Output does not start with 'git' and won't be executed."
+			echo "Generated output: $COMMAND"
 		fi
-  else
-    echo "Error: Couldn't generate a command."
-  fi
+	else
+		echo "Error: Couldn't generate a command."
+	fi
 }
-
